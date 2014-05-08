ad6abec3c974250a6c746ce73725ea80e81ce70d
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2014-05-08 13:37:27 +0100

    Clean up.

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setup(name='cache_flusher',
       author='Roberto Agostino Vitillo',
       author_email='ra.vitillo@gmail.com',
       description='Disk cache flusher',
+      url='https://github.com/vitillo/python_cache_flusher',
       py_modules=['cache_flusher'],
       data_files=[('data', ['data/Flush.exe'])])
