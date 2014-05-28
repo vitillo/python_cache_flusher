b164e2927d5d06e2e2d4103967837c0aa9fa020e
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2014-05-28 18:11:01 +0100

    Move everything to a package.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='cache_flusher',
-      version='1.0.1',
+      version='1.0.2',
       author='Roberto Agostino Vitillo',
       author_email='ra.vitillo@gmail.com',
       description='Disk cache flusher',
