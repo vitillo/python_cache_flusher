451a2ae2334f08d029deff4bcbe4707f82f11c8c
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2014-05-29 15:45:20 +0100

    Quote execution string on Windows to support Command Prompt.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='cache_flusher',
-      version='1.0.2',
+      version='1.0.3',
       author='Roberto Agostino Vitillo',
       author_email='ra.vitillo@gmail.com',
       description='Disk cache flusher',
