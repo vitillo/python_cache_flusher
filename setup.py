6ca70e12e21cfaaeb77f9e7d1d88726d1690be4e
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2014-07-14 14:21:37 +0100

    Increase version number.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='cache_flusher',
-      version='1.0.3',
+      version='1.0.4',
       author='Roberto Agostino Vitillo',
       author_email='ra.vitillo@gmail.com',
       description='Disk cache flusher',
